db7f70d5187bafc24153fea23fb76971feacda1f
jpmejia22 <capitanjp05@gmail.com>
2025-06-02 03:15:23 -0500

    Update VentanaAdmin.java

--- src/AdministradorInterfaz/VentanaAdmin.java
+++ src/AdministradorInterfaz/VentanaAdmin.java
@@ -1,30 +1,79 @@
-package administrador.interfaz;
+package AdministradorInterfaz;
 
+import java.awt.BorderLayout;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import Empleados.Empelado;
 import login.interfaz.VentanaLogin;
+import parqueDeAtracciones.Parque;
+import parqueDeAtracciones.consolaAdministrador;
 
-public class VentanaAdmin extends JFrame{
+
+
+public class VentanaAdmin<Atraccion> extends JFrame{
 	private PanelInferiorAdmin panelIAd;
 	private PanelSuperiorAdmin panelSAd;
 	private PanelCentroAdmin panelCAd;
+	private Parque miParque;
+	private consolaAdministrador cAdmin;
+	
 	private VentanaLogin ventanaLo;
 	
-	public void cerrarSesion()
+	public VentanaAdmin(Parque miParque, consolaAdministrador admin, VentanaLogin ventanaLo) 
 	{
-		try 
-		{
-			ventanaLo.guardarAplicacion();
-		} 
-		catch (FileNotFoundException e) 
-		{
-			JOptionPane.showMessageDialog( this, "Error al guardar los datos" );
-		}
-		this.dispose();
-		ventanaLo.setVisible(true);
+		this.miParque = miParque;
+		this.cAdmin = admin;
+		this.ventanaLo = ventanaLo;
+		
+		setTitle("Parque Atracciones");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(800, 700);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		
+		setLayout( new BorderLayout( ) );
+		
+		panelSAd = new PanelSuperiorAdmin();
+		add(panelSAd, BorderLayout.NORTH);
+		
+		panelCAd = new PanelCentroAdmin(this);
+		add(panelCAd, BorderLayout.CENTER);
+
+		panelIAd = new PanelInferiorAdmin(this);
+		add(panelIAd, BorderLayout.SOUTH);
+	}
+	
+	public void cerrarSesion() {
+        this.dispose();
+        ventanaLo.setVisible(true);
+    }
+	
+	public void asignarTurno() {
 		
 	}
+	private void mostrarMensaje(String string) {
+				
+	}
+	public void consultarTiquetesVendidos() {
+		
+	}
+	public void consultarEspectaculoEspec() {
+		
+	}
+	public void consultarAtraccionEspec() {
+		
+	}
+	public void consultarAtracciones() {
+		
+	}
+	public void consultarEspectaculo() {
+		
+	}
+	private String pedirInput(String mensaje) {
+        return JOptionPane.showInputDialog(this, mensaje);
+    }
 }
